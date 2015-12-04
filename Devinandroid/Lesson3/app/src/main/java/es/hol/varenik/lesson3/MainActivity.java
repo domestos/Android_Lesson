bc1be3f3a1163ec39ace6f40d60246ed7bd3df95
Domestos <elBarto2006@ukr.net>
2015-12-04 21:59:16 +0200

    Devinandroid Lesson3. LinerLayout

---
+++ Devinandroid/Lesson3/app/src/main/java/es/hol/varenik/lesson3/MainActivity.java
@@ -0,0 +1,19 @@
+package es.hol.varenik.lesson3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by varenik on 04.12.15.
+ */
+public class MainActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.main_activiti);
+        super.onCreate(savedInstanceState);
+    }
+}
