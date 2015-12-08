32796588de2c8882ab78b4fb036f567277638672
domestos <elbarto2006@ukr.net>
2015-12-08 16:06:35 +0200

    Devcolibri Design Lesson2 MaterialDesign Ripple - work only <Android 5

---
+++ Devcolibri/Design/Lesson2/app/src/main/java/com/example/vpelenskyi/lesson2/MyRippleActivity.java
@@ -0,0 +1,13 @@
+package com.example.vpelenskyi.lesson2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyRippleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ripple);
+    }
+}
