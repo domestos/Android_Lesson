ad55d981b3b3f89da50de95e4ba90c27a51ecd40
domestos <elbarto2006@ukr.net>
2015-12-09 15:40:22 +0200

    Devcolibri Design Lesson5 Toolbar

---
+++ Devcolibri/Design/Lessno5/app/src/main/java/com/example/vpelenskyi/lessno5/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.vpelenskyi.lessno5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
