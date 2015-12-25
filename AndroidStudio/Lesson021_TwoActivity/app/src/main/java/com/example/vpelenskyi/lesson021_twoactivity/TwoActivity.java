8bf4281a1815593ffb2e462f4a577c476b0d2cfe
v.pelenskyi <elBarto2006@ukr.net>
2015-12-25 16:18:58 +0200

    StartAndroid Lesson021: Activiy, used startActivity

---
+++ AndroidStudio/Lesson021_TwoActivity/app/src/main/java/com/example/vpelenskyi/lesson021_twoactivity/TwoActivity.java
@@ -0,0 +1,15 @@
+package com.example.vpelenskyi.lesson021_twoactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by v.pelenskyi on 25.12.2015.
+ */
+public class TwoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two_activity);
+    }
+}
